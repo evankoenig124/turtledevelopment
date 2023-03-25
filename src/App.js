4c8ef1eddd13c12a67c23cfc2cb33911c741744d
James Treadwell <james.treadwell137@gmail.com>
2023-03-25 14:51:11 -0400

    Added calendar page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React, { useState } from 'react';
-
+import './App.css';
+import { Calendar, dateFinsLoaclizer, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 
 
 const LoginPage = ({ onLogin, onRegister }) => {
@@ -42,6 +49,7 @@ const LoginPage = ({ onLogin, onRegister }) => {
   );
 };
 
+
 const RegisterPage = ({ onRegister }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -73,6 +81,7 @@ const RegisterPage = ({ onRegister }) => {
   );
 };
 
+
 const HomePage = ({ user, onLogout,onViewTasks, onViewCalendar, onViewAccount }) => {
   return (
     <div>
@@ -95,32 +104,78 @@ const HomePage = ({ user, onLogout,onViewTasks, onViewCalendar, onViewAccount })
 };
 
 
-const TaskPage = ({onViewTasks}) =>{
-<div>
-  <h1>Hello!</h1>
-</div>
 
-onViewTasks();
 
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
 }
 
-const CalendarPage = ({onViewCalendar}) =>{
-  <div>
-    <h1>Hello!</h1>
-  </div>
-  
-  onViewCalendar();
-  
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+});
+
+const events = [
+  {
+      title: "Habit 1",
+      allDay: true,
+      start: new Date(2023, 3, 0),
+      end: new Date(2023, 3, 0),
+  },
+  {
+      title: "Habit 2",
+      start: new Date(2023, 3, 7),
+      end: new Date(2023, 3, 10),
+  },
+  {
+      title: "Habit 3",
+      start: new Date(2023, 3, 20),
+      end: new Date(2023, 3, 23),
+  },
+];
+
+const CalendarPage = ({ onViewCalendar}) => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: ""});
+  const [allEvents, setAllEvents] = useState(events)
+
+  function handleAddEvent() {
+    for (let i = 0; i<allEvents.length; i++){
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
+
+      if (
+        ( (d1 <= d2) && (d2 <= d3) ) || ( (d1 <= d4) && (d4 <= d3))
+      )
+      {
+        alert("Clash");
+        break;
+      }
+    }
+    setAllEvents([...allEvents, newEvent]);
   }
 
-const AccountPage = ({onViewAccount}) =>{
-  <div>
-    <h1>Hello!</h1>
-  </div>
-    
-  onViewAccount();
-    
-  }
+  return (
+    <div className="Calendar">
+      <h1>Calendar</h1>
+      <h2>Add New Habit</h2>
+      <div>
+        <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+      </div>
+      <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin:"50px"}} />
+    </div>
+  );
+}
+
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -162,14 +217,8 @@ const App = () => {
       {view === 'home' && (
         <HomePage user={user} onLogout={handleLogout} onViewTasks={handleTasks} onViewCalendar={handleCalendar} onViewAccount={handleAccount}/>
       )}
-      {view === 'tasks'&&(
-        <TaskPage/>
-      )}
       {view === 'calendar'&&(
         <CalendarPage/>
-      )}
-      {view === 'account'&&(
-        <AccountPage/>
       )}
     </div>
     
@@ -177,5 +226,6 @@ const App = () => {
     
   );
 };
+
 
 export default App;
