57b15dde7f9574ef98c7b7e9e1ab393ff0124d00
evankoenig124 <evan.koenig1@gmail.com>
2023-04-23 16:19:09 -0400

    Merge1

--- src/App.js
+++ src/App.js
@@ -176,6 +176,51 @@ const CalendarPage = ({ onViewCalendar}) => {
   );
 }
 
+const AccountPage = () => {
+  const [age, setAge] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleSave = () => {
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    const updatedUser = {
+      ...user,
+      age: age || user.age,
+      birthday: birthday || user.birthday,
+      email: email || user.email,
+      phoneNumber: phoneNumber || user.phoneNumber,
+    };
+    localStorage.setItem('user', JSON.stringify(updatedUser));
+    setAge('');
+    setBirthday('');
+    setEmail('');
+    setPhoneNumber('');
+  };
+
+  return (
+    <section>
+      <h1>Account</h1>
+      <div>
+        <label>Age:</label>
+        <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+      </div>
+      <div>
+        <label>Birthday:</label>
+        <input type="text" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Phone Number:</label>
+        <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+      </div>
+      <button onClick={handleSave}>Save</button>
+    </section>
+  );
+};
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -219,6 +264,9 @@ const App = () => {
       )}
       {view === 'calendar'&&(
         <CalendarPage/>
+      )}
+      {view === 'account' && (
+        <AccountPage/>
       )}
     </div>
     
