19435b564ec967c1e0c8cee0c84c13b227c917dd
evankoenig124 <evan.koenig1@gmail.com>
2023-04-23 15:40:51 -0400

    Merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
-import TaskPage from './components/TaskPage';
-import AccountPage from './components/AccountPage'
+import './App.css';
+import { Calendar, dateFinsLoaclizer, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 
 
 const LoginPage = ({ onLogin, onRegister }) => {
@@ -43,6 +49,7 @@ const LoginPage = ({ onLogin, onRegister }) => {
   );
 };
 
+
 const RegisterPage = ({ onRegister }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -74,19 +81,20 @@ const RegisterPage = ({ onRegister }) => {
   );
 };
 
-const HomePage = ({ user, onLogout, onViewTasks, onViewCalendar, onViewAccount }) => {
+
+const HomePage = ({ user, onLogout,onViewTasks, onViewCalendar, onViewAccount }) => {
   return (
     <div>
-      <div className="home-container">
-        <div className="home-header">
-          <h3 className="home-title">Welcome, {user.username}!</h3>
-          <button className="logout-button" onClick={onLogout}>Logout</button>
-        </div>
-        <div className="home-body">
-          <div className="home-sidebar">
-            <button className="sidebar-button" onClick={() => onViewTasks()}>Tasks</button>
-            <button className="sidebar-button" onClick={onViewCalendar}>Calendar</button>
-            <button className="sidebar-button" onClick={() => onViewAccount()}>Account</button>
+    <div className="home-container">
+      <div className="home-header">
+        <h3 className="home-title">Welcome, {user.username}!</h3>
+        <button className="logout-button" onClick={onLogout}>Logout</button>
+      </div>
+      <div className="home-body">
+        <div className="home-sidebar">
+          <button className="sidebar-button" onClick={() => onViewTasks()}>Tasks</button>
+          <button className="sidebar-button" onClick={onViewCalendar}>Calendar</button>
+          <button className="sidebar-button" onClick={onViewAccount}>Account</button>
           </div>
         </div>
         <div className="home-content"></div>
@@ -96,89 +104,176 @@ const HomePage = ({ user, onLogout, onViewTasks, onViewCalendar, onViewAccount }
 };
 
 
-function TasksPage(props) {
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+});
+
+const events = [
+  {
+      title: "Habit 1",
+      allDay: true,
+      start: new Date(2023, 3, 0),
+      end: new Date(2023, 3, 0),
+  },
+  {
+      title: "Habit 2",
+      start: new Date(2023, 3, 7),
+      end: new Date(2023, 3, 10),
+  },
+  {
+      title: "Habit 3",
+      start: new Date(2023, 3, 20),
+      end: new Date(2023, 3, 23),
+  },
+];
+
+const CalendarPage = ({ onViewCalendar}) => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: ""});
+  const [allEvents, setAllEvents] = useState(events)
+
+  function handleAddEvent() {
+    for (let i = 0; i<allEvents.length; i++){
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
+
+      if (
+        ( (d1 <= d2) && (d2 <= d3) ) || ( (d1 <= d4) && (d4 <= d3))
+      )
+      {
+        alert("Clash");
+        break;
+      }
+    }
+    setAllEvents([...allEvents, newEvent]);
+  }
+
   return (
-    <div>
-      <h1>Tasks Page</h1>
-      <p>Welcome to the Tasks page.</p>
-      <button onClick={() => props.setPage("home")}>Home</button>
+    <div className="Calendar">
+      <h1>Calendar</h1>
+      <h2>Add New Habit</h2>
+      <div>
+        <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+      </div>
+      <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin:"50px"}} />
     </div>
   );
 }
 
-const CalendarsPage = ({onViewCalendar}) =>{
-  <div>
-    <h1>Hello!</h1>
-  </div>
-  
-  onViewCalendar();
-  
-  }
+const AccountPage = () => {
+  const [age, setAge] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
-  function AccountsPage(props) {
-    return (
-      <div>
-        <h1>Account Page</h1>
-        <p>Welcome to the Tasks page.</p>
-        <button onClick={() => props.setPage("home")}>Home</button>
-      </div>
-    );
-  }
-
-  const App = () => {
-    const [user, setUser] = useState(null);
-    const [view, setView] = useState('login');
-  
-    const handleLogin = (user) => {
-      setUser(user);
-      setView('home');
+  const handleSave = () => {
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    const updatedUser = {
+      ...user,
+      age: age || user.age,
+      birthday: birthday || user.birthday,
+      email: email || user.email,
+      phoneNumber: phoneNumber || user.phoneNumber,
     };
-  
-    const handleRegister = () => {
-      setView('register');
-    };
-  
-    const handleLogout = () => {
-      setUser(null);
-      setView('login');
-    };
-  
-    const handleTasks = () => {
-      setView('tasks');
-    };
-  
-    const handleCalendar = () => {
-      setView('calendar');
-    };
-  
-    const handleAccount = () => {
-      setView('account');
-    };
-  
-    return (
-      <section>
-        <div>
-          {view === 'login' && (
-            <LoginPage onLogin={handleLogin} onRegister={handleRegister} />
-          )}
-          {view === 'register' && (
-            <RegisterPage onRegister={() => setView('login')} />
-          )}
-          {view === 'home' && (
-            <HomePage
-              user={user}
-              onLogout={handleLogout}
-              onViewTasks={handleTasks}
-              onViewCalendar={handleCalendar}
-              onViewAccount={handleAccount}
-            />
-          )}
-          {view === 'tasks' && <TaskPage onViewTasks={handleTasks} />}
-          {view === 'calendar' && <CalendarsPage onViewCalendar={handleCalendar} />}
-          {view === 'account' && <AccountPage onViewAccount={handleAccount} />}
-        </div>
-      </section>
-    );
+    localStorage.setItem('user', JSON.stringify(updatedUser));
+    setAge('');
+    setBirthday('');
+    setEmail('');
+    setPhoneNumber('');
   };
+
+  return (
+    <section>
+      <h1>Account</h1>
+      <div>
+        <label>Age:</label>
+        <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+      </div>
+      <div>
+        <label>Birthday:</label>
+        <input type="text" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Phone Number:</label>
+        <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+      </div>
+      <button onClick={handleSave}>Save</button>
+    </section>
+  );
+};
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [view, setView] = useState('login');
+
+  const handleLogin = (user) => {
+    setUser(user);
+    setView('home');
+  };
+
+  const handleRegister = () => {
+    setView('register');
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    setView('login');
+  };
+  const handleTasks = () =>{
+    setView('tasks');
+  }
+  const handleCalendar = () =>{
+    setView('calendar');
+  }
+  const handleAccount = () =>{
+    setView('account');
+  }
+
+  return (
+    <section>
+    
+    <div>
+      {view === 'login' && (
+        <LoginPage onLogin={handleLogin} onRegister={handleRegister} />
+      )}
+      {view === 'register' && (
+        <RegisterPage onRegister={() => setView('login')} />
+      )}
+      {view === 'home' && (
+        <HomePage user={user} onLogout={handleLogout} onViewTasks={handleTasks} onViewCalendar={handleCalendar} onViewAccount={handleAccount}/>
+      )}
+      {view === 'calendar'&&(
+        <CalendarPage/>
+      )}
+      {view === 'account' && (
+        <AccountPage/>
+      )}
+    </div>
+    
+    </section>
+    
+  );
+};
+
 
 export default App;
